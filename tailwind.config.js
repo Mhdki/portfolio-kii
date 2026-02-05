3df21b21995d65c5cac9de7fc519b1d7a3e35858
mhdkii <exchangepro19@gmail.com>
2026-02-05 11:53:30 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Plus Jakarta Sans"', 'sans-serif'], // Font modern mirip desain
+      },
+      colors: {
+        'brand-purple': '#5D3FD3', // Ungu utama
+        'brand-dark': '#1A1A2E',   // Ungu gelap untuk background
+        'brand-lime': '#CCF381',   // Hijau stabilo (Lime)
+        'brand-white': '#F8F9FA',
+      },
+      backgroundImage: {
+        'pattern': "url('https://www.transparenttextures.com/patterns/cubes.png')",
+      }
+    },
   },
   plugins: [],
 }
