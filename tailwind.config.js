1f121143aefa00d751f76ad1e481af29903a9d45
mhdkii <exchangepro19@gmail.com>
2026-02-05 12:02:59 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,23 +8,16 @@ export default {
     extend: {
       fontFamily: {
         'sans': ['"Plus Jakarta Sans"', 'sans-serif'],
-        'display': ['"Plus Jakarta Sans"', 'sans-serif'], // Pakai font modern yang sama
+        'display': ['"Plus Jakarta Sans"', 'sans-serif'], 
       },
       colors: {
-        'brand-purple': '#5D3FD3', 
-        'brand-dark': '#0F0F1A',   // Lebih gelap dikit biar pop
-        'brand-lime': '#CCF381',   
+        'brand-purple': '#5D3FD3', // Ungu Utama
+        'brand-dark': '#0F0F1A',   // Hitam Kebiruan
+        'brand-lime': '#CCF381',   // Hijau Stabilo
         'brand-white': '#F8F9FA',
       },
       animation: {
         'spin-slow': 'spin 10s linear infinite',
-        'float': 'float 6s ease-in-out infinite',
-      },
-      keyframes: {
-        float: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-20px)' },
-        }
       },
       backgroundImage: {
         'noise': "url('https://grainy-gradients.vercel.app/noise.svg')",
